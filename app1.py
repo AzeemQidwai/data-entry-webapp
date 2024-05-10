b501d96a5d690325cc792cf8cedc10e71f799c81
A.Amjad <aamjad@khi.iba.edu.pk>
2024-05-10 18:30:44 +0500

    Update app1.py
    Added database variables

--- app1.py
+++ app1.py
@@ -1,7 +1,15 @@
 #pip install flask
-
+#pip install pyodbc
 
 from flask import Flask, render_template, request, redirect, url_for, flash
+import pyodbc
+import os
+
+SERVER = os.environ['SERVER']
+DATABASE = os.environ['DATABASE']
+USERNAME = os.environ['NAME']
+PASSWORD = os.environ['PASSWORD']
+
 
 app = Flask(__name__)
 app.secret_key = 'super_secret_key'  # Required for session management (e.g., flashing messages)
@@ -14,13 +22,37 @@ def index():
 # Route to handle form submissions
 @app.route('/submit_review', methods=['POST'])
 def submit_review():
-    name = request.form['name']
-    email = request.form['email']
-    review = request.form['review']
-    rating = request.form['rating']
+    try:
+        name = request.form['name']
+        email = request.form['email']
+        review = request.form['review']
+        rating = request.form['rating']
 
-    # Here, you would typically save these data to a database
-    # For this example, we'll just flash a message and redirect to the form page
+        # Here, you would typically save these data to a database
+        # Azure SQL connection string
+        connectionString = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
+
+        # Establish connection
+        conn = pyodbc.connect(connectionString) 
+
+        # Create cursor
+        cursor = conn.cursor()
+
+        # Insert data into table
+        cursor.execute(
+            "INSERT INTO test_table (CustomerName, CustomerEmail, Review, Rating) VALUES (?, ?, ?, ?))",
+            name, email, review, rating
+        )
+
+        # Commit transaction
+        conn.commit()
+
+        print('Data stored successfully')
+    
+    except Exception as e:
+        print(f'Error: {str(e)}')
+
+        # redirect to the thank you page
 
     return redirect(url_for('thank_you', name=name))
 
